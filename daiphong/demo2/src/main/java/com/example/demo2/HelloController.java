65e33c21cb7f88ac64cb5fa4637dbfe3949d89a7
TTDucAI18 <160409094+TTDucAI18@users.noreply.github.com>
2024-04-02 21:33:51 +0700

    Create HelloController.java

---
+++ daiphong/demo2/src/main/java/com/example/demo2/HelloController.java
@@ -0,0 +1,81 @@
+package com.example.demo2;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.lang.*;
+
+public class HelloController {
+    @FXML
+    private TextField textField;
+    @FXML
+    private ListView listView;
+    @FXML
+    private ListView textArea;
+    HashMap<String,String> words = new HashMap<>();
+    @FXML
+    public void onOpenClick(){
+        try {
+            String filePath = "D:\\code\\oop\\demo2\\src\\main\\java\\com\\example\\demo2\\E_V.txt";
+
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            StringBuilder content = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+            reader.close();
+
+            // Tách các từ điển từ nội dung HTML
+            Pattern pattern = Pattern.compile("<html><i>(.*?)</i><br/><ul><li><b><i>(.*?)</i></b><ul><li><font color='#cc0000'><b>(.*?)</b></font>");
+            Matcher matcher = pattern.matcher(content.toString());
+
+            String[] parts = matcher.pattern().split("<html>");
+            // Duyệt qua các kết quả và in ra màn hình
+            while (matcher.find()) {
+                String word = matcher.group(1);
+                String meaning = matcher.group(3);
+                words.put(word,meaning);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    public void onClick(){
+        String typedWord = textField.getText();
+        for (String key:words.keySet()){
+            if (key.charAt(0) == typedWord.charAt(0)){
+                listView.getItems().add(key);
+            }
+        }
+    }
+    @FXML
+    public void OnSearchClick(){
+        String typedWord = textField.getText();
+        for (String key:words.keySet()){
+            if (key.contains(typedWord)){
+                textArea.getItems().add("  -  "+key);
+                textArea.getItems().add("        "+words.get(key));
+                // listView.getItems().add(key); can phai them vao dau
+            }
+        }
+    }
+}
