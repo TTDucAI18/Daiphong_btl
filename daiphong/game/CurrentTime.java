7037f07952d96513ad085686df9bf02ce48b2770
TTDucAI18 <160409094+TTDucAI18@users.noreply.github.com>
2024-05-08 22:33:38 +0700

    Create CurrentTime.java

---
+++ daiphong/game/CurrentTime.java
@@ -0,0 +1,17 @@
+package org.example.demo9;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class CurrentTime {
+
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+    LocalDateTime now = LocalDateTime.now();
+
+    public CurrentTime(){
+    }
+
+    public String currentTime(){
+        return dtf.format(now);
+    }
+}
