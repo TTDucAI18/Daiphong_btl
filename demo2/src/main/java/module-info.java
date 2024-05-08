492613790532ebc91566b62c61601dfc35d7413d
TTDucAI18 <160409094+TTDucAI18@users.noreply.github.com>
2024-05-08 22:43:35 +0700

    Create module-info.java

---
+++ demo2/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.demo2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+    requires com.almasb.fxgl.all;
+    requires java.net.http;
+    requires jsapi;
+    requires javafx.media;
+    requires java.xml;
+//    requires jsapi;
+
+    opens com.example.demo2 to javafx.fxml;
+    exports com.example.demo2;
+}
